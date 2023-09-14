5d53df4455c9073d89e7630fd4f4adf0baf43bd4
SiuL06 <144299790+SiuL06@users.noreply.github.com>
2023-09-14 11:57:59 +0800

    third commit

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
       <li>9. Being Prepared</li>
       <li>10. Having A Strong Work Ethic</li>
     </ul>
-    </p>
+        <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
